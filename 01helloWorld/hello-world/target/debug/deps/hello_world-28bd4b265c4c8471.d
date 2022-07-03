d0939de3392dca19eda8e4ca7b5697bc4066a3ec
lucasbiagetti <lucasbiagetti@gmail.com>
2022-07-03 16:38:59 -0300

    cargo

---
+++ 01helloWorld/hello-world/target/debug/deps/hello_world-28bd4b265c4c8471.d
@@ -0,0 +1,5 @@
+/home/lucas/gitProjects/rustLearningPath/01helloWorld/hello-world/target/debug/deps/hello_world-28bd4b265c4c8471: src/main.rs
+
+/home/lucas/gitProjects/rustLearningPath/01helloWorld/hello-world/target/debug/deps/hello_world-28bd4b265c4c8471.d: src/main.rs
+
+src/main.rs:
---
+++ 01helloWorld/hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/home/lucas/gitProjects/rustLearningPath/01helloWorld/hello-world/target/debug/hello-world: /home/lucas/gitProjects/rustLearningPath/01helloWorld/hello-world/src/main.rs
